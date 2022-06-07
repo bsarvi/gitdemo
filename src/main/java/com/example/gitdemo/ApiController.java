4d777708a041e6a4574cd572b2ff1d56a3dfb9c3
bsarvi <bharasarvi@gmail.com>
2022-06-07 16:51:52 +0530

    initial commit

---
+++ src/main/java/com/example/gitdemo/ApiController.java
@@ -0,0 +1,11 @@
+package com.example.gitdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping
+    public String hello(){return "Hello World";}
+}
